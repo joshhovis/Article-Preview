09e7efc1d5298b1c88e3afed7ff06743cadf5776
joshhovis <joshuahovis3@gmail.com>
2024-07-15 16:41:50 -0400

    Finish desktop view and button logic

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const socialShareBtn = document.querySelector('.share-arrow');
     const authorDiv = document.getElementById('author');
     const shareDiv = document.getElementById('share');
+    const authorShareArrowIcon = document.querySelector('.author-share-icon');
+
+    const filterWhite = 'invert(100%) sepia(100%) saturate(0%) hue-rotate(224deg) brightness(200%) contrast(102%);'
+
+    let shareVisible = false;
     
     if (window.innerWidth <= 768) {
         authorShareBtn.addEventListener('click', () => {
@@ -14,6 +19,31 @@ document.addEventListener("DOMContentLoaded", () => {
             authorDiv.style = 'display: flex;'
             shareDiv.style = 'display: none;'
         })
-    }
+    } else {
+        authorShareBtn.addEventListener('click', () => {
+            shareVisible = !shareVisible;
+            if (shareVisible) {
+                authorShareArrowIcon.style = `filter:${filterWhite}`
+                authorShareBtn.style = 'background-color: #6E8098'
+                shareDiv.style = 'display: flex;'
+            } else {
+                resetShareDiv();
+            }
+        });
 
+        document.addEventListener('click', (e) => {
+            if (!authorShareBtn.contains(e.target) && !shareDiv.contains(e.target)) {
+                if (shareVisible) {
+                    resetShareDiv();
+                }
+            }
+        });
+
+        const resetShareDiv = () => {
+            shareDiv.style.display = 'none';
+            authorShareArrowIcon.style.filter = '';
+            authorShareBtn.style.backgroundColor = '#ECF2F8'
+            shareVisible = false;
+        };
+    }
 });
